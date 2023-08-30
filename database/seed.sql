7037c1e7e564f45d90cc8955de5435ac6c9604eb
JoaoxMari <secundaria618@gmail.com>
2023-08-30 08:18:12 -0300

    fix: fix 'seed' and 'schemaModifications' database files and create first house record with empty diagram_positions

--- database/seed.sql
+++ database/seed.sql
@@ -1,12 +1,12 @@
 USE lar;
 
 -- Create initial values for database
-INSERT INTO Houses(house_id) VALUES (1);
+INSERT INTO Houses(diagram_positions) VALUES ('{}');
 
 INSERT INTO Users(fk_house_id, user_email, user_password, first_name, last_name) 
 VALUES (1, 'rodrigosilva@gmail.com', '12345', 'Rodrigo', 'Silva');
 
-INSERT INTO Rooms(fk_house_id, room_name) VALUES 
+INSERT INTO Rooms(fk_house_id, room_name, fk_tile_id) VALUES 
     (1, 'Cozinha', 2),
     (1, 'Quarto', 3),
     (1, 'Sala de Estar', 4),
