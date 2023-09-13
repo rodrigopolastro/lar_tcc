25217cee7758cccd05862846bea413b2b1e31f37
Rodrigo Polastro da Silva <rodrigo.polastro03@gmail.com>
2023-09-13 10:08:22 -0300

    store furniture images in file system and create 'furniture_images' table

--- assets/js/construction/furnitureEditor.js
+++ assets/js/construction/furnitureEditor.js
@@ -1,13 +1,1 @@
 
-
-chairImg.addEventListener('click', () => {
-  selectedFurniture.imgElement = chairImg;
-  selectedFurniture.width = this.width;
-  selectedFurniture.height = this.height;
-})
-
-benchImg.addEventListener('click', () => {
-  selectedFurniture.imgElement = benchImg;
-  selectedFurniture.width = this.width;
-  selectedFurniture.height = this.height;
-})
