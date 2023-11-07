eddda27cf1402eaa9cf0337341c08e0e388c1792
ETIM DS 2021 <etim2021.ds@etecmogiguacu.com.br>
2023-11-07 08:48:06 -0300

    Fixed show task spacing

--- views/tasks/createTaskDiv.js
+++ views/tasks/createTaskDiv.js
@@ -1,18 +1,20 @@
 function createTaskDiv(task){
 
-  const taskDiv             = createElementWithAttributes('div', {class: 'rounded-4 mb-2'});
-  const outerTaskDiv01      = createElementWithAttributes('div', {class: 'row'});
+  const taskDiv             = createElementWithAttributes('div', {class: 'rounded-4 mb-3'});
+  const outerTaskDiv01      = createElementWithAttributes('div', {class: 'row py-2'});
   const outerTaskDiv02      = createElementWithAttributes('div', {class: 'row'});
   const outerTaskDiv03      = createElementWithAttributes('div', {class: 'row'});
   const innerTaskDiv01      = createElementWithAttributes('div', {class: 'col-1'});
-  const innerTaskDiv02      = createElementWithAttributes('div', {class: 'col me-5'});
-  const innerTaskDiv03      = createElementWithAttributes('div', {class: 'col ms-5'});
+  const innerTaskDiv02      = createElementWithAttributes('div', {class: 'col-5 pe-5 me-5'});
+  const innerTaskDivEdit    = createElementWithAttributes('div', {class: 'col d-flex justify-content-end ms-5 ps-5'});
+  const innerTaskDiv03      = createElementWithAttributes('div', {class: 'col d-flex justify-content-end'});
+  const innerTaskDivDrop    = createElementWithAttributes('div', {class: 'col d-flex justify-content-end'});
   const innerTaskDiv04      = createElementWithAttributes('div', {class: 'col ms-1'});
   const innerTaskDiv05      = createElementWithAttributes('div', {class: 'col'});
-  const innerTaskDiv06      = createElementWithAttributes('div', {class: 'col'});
+  const innerTaskDiv06      = createElementWithAttributes('div', {class: 'col-1'});
   const innerTaskDiv07      = createElementWithAttributes('div', {class: 'col ms-1'});
   const innerTaskDiv08      = createElementWithAttributes('div', {class: 'col'});
-  const innerTaskDiv09      = createElementWithAttributes('div', {class: 'col'});
+  const innerTaskDiv09      = createElementWithAttributes('div', {class: 'col-1'});
   const shownTaskDiv        = createElementWithAttributes('div', {class: ''})
   const hiddenInnerTaskDiv  = createElementWithAttributes('div', {class: 'card-body'});
   const hiddenOuterTaskDiv  = createElementWithAttributes('div', {class: 'collapse', id: task.task_id});
@@ -23,7 +25,7 @@ function createTaskDiv(task){
   const taskRoom        = createElementWithAttributes('p',  {});
   const taskFurniture   = createElementWithAttributes('p',  {});
   const taskCollapse    = document.createElement("a");
-  taskCollapse.setAttribute('class', 'btn ms-5');
+  taskCollapse.setAttribute('class', 'btn');
   taskCollapse.setAttribute('data-bs-toggle', 'collapse');
   taskCollapse.setAttribute('href', '#' + task.task_id);
 
@@ -43,13 +45,15 @@ function createTaskDiv(task){
   const formDeleteTask  = createElementWithAttributes('form', {method: 'post', action: '.'});
   const deleteTaskId    = createElementWithAttributes('input', {type: 'hidden', name: 'task_id', value: task.task_id});
   const deleteOperation = createElementWithAttributes('input', {type: 'hidden', name: 'operation', value: 'deleteTask'});
-  const deleteSubmit    = createElementWithAttributes('input', {type: 'image', src: '../../assets/images/icons/trash.png', value: 'Excluir Tarefa', class:'ms-2'});     
+  const deleteSubmit    = createElementWithAttributes('input', {type: 'image', src: '../../assets/images/icons/trash.png', value: 'Excluir Tarefa', class:'btn'});     
 
   // Stablish hierarchy between elements
   taskDiv.appendChild(outerTaskDiv01);
   outerTaskDiv01.appendChild(innerTaskDiv01);
   outerTaskDiv01.appendChild(innerTaskDiv02);
+  outerTaskDiv01.appendChild(innerTaskDivEdit);
   outerTaskDiv01.appendChild(innerTaskDiv03);
+  outerTaskDiv01.appendChild(innerTaskDivDrop);
   outerTaskDiv01.appendChild(hiddenOuterTaskDiv).appendChild(hiddenInnerTaskDiv);
   hiddenInnerTaskDiv.appendChild(outerTaskDiv02);
   hiddenInnerTaskDiv.appendChild(outerTaskDiv03);
@@ -60,14 +64,15 @@ function createTaskDiv(task){
   innerTaskDiv01.appendChild(formCompleteTask);
   innerTaskDiv02.appendChild(taskName).appendChild(taskNameText);
 
-  innerTaskDiv03.appendChild(taskCollapse).appendChild(taskCollapseImg);
+  innerTaskDivDrop.appendChild(taskCollapse).appendChild(taskCollapseImg);
 
+  outerTaskDiv02.appendChild(innerTaskDiv06);
   outerTaskDiv02.appendChild(innerTaskDiv04);
   outerTaskDiv02.appendChild(innerTaskDiv05);
-  outerTaskDiv02.appendChild(innerTaskDiv06);
+
+  outerTaskDiv03.appendChild(innerTaskDiv09);
   outerTaskDiv03.appendChild(innerTaskDiv07);
   outerTaskDiv03.appendChild(innerTaskDiv08);
-  outerTaskDiv03.appendChild(innerTaskDiv09);
 
 
 
@@ -81,7 +86,7 @@ function createTaskDiv(task){
   formDeleteTask.appendChild(deleteTaskId);
   formDeleteTask.appendChild(deleteOperation);
   formDeleteTask.appendChild(deleteSubmit);
-  innerTaskDiv09.appendChild(formDeleteTask);
+  innerTaskDiv03.appendChild(formDeleteTask);
 
   if (task.is_completed == true){
     taskDiv.classList.add('bg-green')
@@ -95,12 +100,12 @@ function createTaskDiv(task){
     taskDiv.classList.add('bg-red')
     taskDiv.classList.add('border-red')
     // Only uncompleted tasks can be edited
-    const buttonEditTask = createElementWithAttributes('button', {onclick:"selectOneTask(" + task.task_id + ")", class: 'border border-0 btn btn-link'});
+    const buttonEditTask = createElementWithAttributes('button', {onclick:"selectOneTask(" + task.task_id + ")", class: 'border-0 btn btn-link'});
     const buttonEditImage  = createElementWithAttributes('img', {src: '../../assets/images/icons/pen.png',});
     // Bootstrap attributes for triggering modal
     buttonEditTask.dataset.bsToggle = 'modal';
     buttonEditTask.dataset.bsTarget = '#staticBackdrop';
-    innerTaskDiv06.appendChild(buttonEditTask);
+    innerTaskDivEdit.appendChild(buttonEditTask);
     buttonEditTask.appendChild(buttonEditImage);
 
     completeOperation.value = 'completeTask';
