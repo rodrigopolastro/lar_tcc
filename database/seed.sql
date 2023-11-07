9726b6a34127b16fa43fb6f3e4aeeedbf66d0bae
internet info <internet.info@etecmogiguacu.com.br>
2023-11-07 08:16:30 -0300

    fix: add wall_id foreign key in rooms creation seed file

--- database/seed.sql
+++ database/seed.sql
@@ -17,9 +17,9 @@ INSERT INTO Houses(diagram_positions) VALUES ('{
 INSERT INTO Users(fk_house_id, user_email, user_password, first_name, last_name) 
 VALUES (1, 'rodrigosilva@gmail.com', '12345', 'Rodrigo', 'Silva');
 
-INSERT INTO Rooms(fk_house_id, room_name, fk_tile_id) VALUES 
-    (1, 'Cozinha', 2),
-    (1, 'Quarto', 3),
-    (1, 'Sala de Estar', 4),
-    (1, 'Sala de Jantar', 5),
-    (1, 'Banheiro', 6);
+INSERT INTO Rooms(fk_house_id, room_name, fk_tile_id, fk_wall_id) VALUES 
+    (1, 'Cozinha', 1, 1),
+    (1, 'Quarto', 2, 2),
+    (1, 'Sala de Estar', 3, 3),
+    (1, 'Sala de Jantar', 4, 4),
+    (1, 'Banheiro', 5, 5);
