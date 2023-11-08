fb703c4a363031aabbbccf9bea46cb1c64781d61
Rodrigo Polastro ESCOLA <rodrigo.polastro03@gmail.com>
2023-11-08 10:50:34 -0300

    style: display rooms in more organized way and fix wallDiv size

--- views/construction/diagramImages/createTileImg.js
+++ views/construction/diagramImages/createTileImg.js
@@ -1,5 +1,5 @@
 function createTileImg(tile, parentElement, tileDisplayElement){
-  const tileImage = createElementWithAttributes('img', {src: findTilePath(tile.tile_name), class:'tile-image ms-3 mb-3'});
+  const tileImage = createElementWithAttributes('img', {src: findTilePath(tile.tile_name), class:'tile-image rounded-2 ms-3 mb-3'});
 
   tileImage.addEventListener('click', function() {
     setRoomTile(tile.tile_id, tile.tile_name, tileDisplayElement)
--- views/construction/rooms/createRoomDiv.js
+++ views/construction/rooms/createRoomDiv.js
@@ -1,21 +1,30 @@
 function createRoomDiv(room){
-  const roomDiv = createElementWithAttributes('div', {class:'d-flex align-items-center bg-cream border-brown rounded-5 pt-2 pb-2 pe-2 mb-3 row'});
+  const roomDiv = createElementWithAttributes('div', {class:'d-flex align-items-center bg-cream border-brown rounded-5 pt-2 pb-3 px-3 mb-3 mx-3 row', style:"width: 45%"});
   roomDiv.dataset.roomId = room.room_id;
 
-  const roomNameElement  = createElementWithAttributes('h4',      {class:'txt-brown fw-bold'});
+  const roomNameElement  = createElementWithAttributes('h4',{class:'txt-brown fw-bold'});
   const roomNameText     = document.createTextNode(room.room_name);
-  const roomTileDiv      = createElementWithAttributes('div', {class:'col'})
-  const roomTileImg      = createElementWithAttributes('img', {src: findTilePath(room.tile_name), class:'tile-image ms-2'});
-  const roomWallDiv      = createElementWithAttributes('div', {class:'col wall-image rounded-2 d-flex align-items-center justify-content-center overflow-hidden'})
+
+  const roomCenterRow    = createElementWithAttributes('div', {class:'d-flex'})
+  
+  const roomTileDiv      = createElementWithAttributes('div', {class:'col  me-3'})
+  const tileNameElement  = createElementWithAttributes('h6', {class:'txt-brown text-center fw-bold'});
+  const tileNameText     = document.createTextNode('Piso');
+
+  const roomTileImg      = createElementWithAttributes('img', {src: findTilePath(room.tile_name), class:'tile-image rounded-2 mb-1'});
+  const roomWallDiv      = createElementWithAttributes('div', {class: 'col '})
+  const wallNameElement  = createElementWithAttributes('h6', {class:'txt-brown text-center fw-bold'});
+  const wallNameText     = document.createTextNode('Parede');
+  const roomWallImageDiv = createElementWithAttributes('div', {class:'col wall-image rounded-2 d-flex align-items-center justify-content-center overflow-hidden'})
   const roomWallImg      = createElementWithAttributes('img', {src: findWallPath(room.wall_name), class:'', height: "400px"});
-  const buttonsDiv = createElementWithAttributes('div', {class:'row'}) 
-  const buttonDeleteRoom = createElementWithAttributes('button', {class:'border border-0 h5 btn btn-link col mx-5'});
-  const buttonEditRoom   = createElementWithAttributes('button', {class:'border border-0 h5 btn btn-link col mx-5'});
+
+  const buttonDeleteRoom = createElementWithAttributes('button', {class:'border border-0 h5 btn btn-link col'});
+  const buttonEditRoom   = createElementWithAttributes('button', {class:'border border-0 h5 btn btn-link col'});
   const buttonDeleteImg  =  createElementWithAttributes('img',   {src: '../../assets/images/icons/trash.png'});
   const buttonEditImg    =  createElementWithAttributes('img',   {src: '../../assets/images/icons/pen.png'});
 
   roomTileImg.addEventListener('mousedown', function(){ setRoomTileToPaint(room.room_id, roomTileImg) });
-  roomWallImg.addEventListener('mousedown', function(){ setRoomWallToPaint(room.room_id, roomWallDiv) });
+  roomWallImg.addEventListener('mousedown', function(){ setRoomWallToPaint(room.room_id, roomWallImageDiv) });
 
   //Only allow room deletion if there are no furniture on it
   buttonDeleteRoom.addEventListener('click', function(){ selectFurnitureOfRoom(room.room_id) });
@@ -27,11 +36,15 @@ function createRoomDiv(room){
   buttonEditRoom.dataset.bsTarget = '#editingRoomModal';
 
   roomDiv.appendChild(roomNameElement).appendChild(roomNameText);
-  roomDiv.appendChild(roomTileDiv).appendChild(roomTileImg);
-  roomDiv.appendChild(roomWallDiv).appendChild(roomWallImg);
-  buttonsDiv.appendChild(buttonDeleteRoom).appendChild(buttonDeleteImg);
-  buttonsDiv.appendChild(buttonEditRoom).appendChild(buttonEditImg);
-  roomDiv.appendChild(buttonsDiv)
+  roomTileDiv.appendChild(roomTileImg);
+  roomTileDiv.appendChild(tileNameElement).appendChild(tileNameText)
+  roomWallDiv.appendChild(roomWallImageDiv).appendChild(roomWallImg);
+  roomWallDiv.appendChild(wallNameElement).appendChild(wallNameText)
+  roomCenterRow.appendChild(roomTileDiv)
+  roomCenterRow.appendChild(roomWallDiv)
+  roomCenterRow.appendChild(buttonDeleteRoom).appendChild(buttonDeleteImg);
+  roomCenterRow.appendChild(buttonEditRoom).appendChild(buttonEditImg);
+  roomDiv.appendChild(roomCenterRow)
 
   roomsList.appendChild(roomDiv);
 }
