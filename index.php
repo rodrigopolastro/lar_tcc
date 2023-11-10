34f083c1f3cf476623504747d7fa76aa08a9c69a
internet info <internet.info@etecmogiguacu.com.br>
2023-11-10 14:36:28 -0300

    Adding the first part of the landing page

--- index.php
+++ index.php
@@ -25,30 +25,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <meta charset="UTF-8">
+<meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Includes bootstrap css -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <!-- My CSS -->
+  <link rel="stylesheet" href="/htdocsDirectories/lar_tcc/assets/css/construction.css">
+  <link rel="stylesheet" href="/htdocsDirectories/lar_tcc/assets/css/tasks.css">
   <title>Organize seu Lar!</title>
 </head>
-<body>
-  <header> 
-    <h1> Organize seu Lar! </h1>
-    <h3>Por enquanto, o login sempre passa o user_id = 1</h3>
-    <form action="." method="post">
-      <input type="hidden" name="mode" value="tasks">
-      <input type="hidden" name="user_id" value="<?= $logged_user_id ?>">
-      <input type="submit" value="Login">
-    </form>
-    <form action="." method="post">
-      <input type="submit" value="Cadastre-se">
-    </form>
-    <hr>
+<body class="container-fluid" style="max-width: 1460px">
+  <header class="mt-2 d-flex justify-content-center align-items-center bg-yellow" style="height: 10vh; width: 100%;">
+    <div class="d-flex justify-content-between" style="width:90%;">
+      <img src="/htdocsDirectories/lar_tcc/assets/images/logo.svg" alt="">
+      <div>
+        <a href="login.html" class="bg-brown rounded-5 txt-pink py-2 px-4 fs-3 me-3 fw-bold link-underline link-underline-opacity-0">Login</a>
+        <a href="signup.html" class="txt-brown fs-3 fw-bold" >ou cadastre-se</a>
+      </div>
+      </div>
   </header>
   <main>
-  <h1> Venha organizar seu Lar aqui</h1>
-  <h3> Um aplicativo de tarefas voltado a organização da casa!</h3>
-  <h3> Simples, intuitivo e com um diagrama de personalização dinâmico!</h3>
-  <img src="https://placeholder.co/500" alt="">
+    <div class="w-100 vh-100 bg-img-landing text-center txt-brown" style="">
+      <h1 class="fw-bold"> Deixe seu <span class="txt-red">LAR</span> um Lugar mais Leve: <br> Organize tudo enquanto se diverte!</h1>
+      <h3 class="txt-red"> Gerencie suas tarefas domésticas de modo <br> eficaz e prazeroso com o sistema LAR</h3>
+      
+    </div>
 <?php require findPath('views/components/footer.html'); ?>
  
 
