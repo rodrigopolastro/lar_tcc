cd2580e19fa4d2db31e7b6b2615e47892db146bb
otavio <ninefawn9@gmail.com>
2023-11-23 23:05:15 -0300

    Adding local bootstrap

--- views/tasks/index.php
+++ views/tasks/index.php
@@ -22,7 +22,7 @@
   require findPath('views/components/footer.html'); 
 ?>
   <!-- Includes bootstrap javascript -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <script src="../../assets/js/bootstrap.bundle.min.js"></script>
 
   <!-- Global vars and consts Declaration -->
   <script src="/htdocsDirectories/lar_tcc/assets/js/tasks/globalDeclarations.js"></script>
