aa7fe66e686535125a266d1a2d4855e04b6d2813
otavio <ninefawn9@gmail.com>
2023-11-23 21:52:16 -0300

    Adding spacing and right sizes in login and signup pages

--- views/login.php
+++ views/login.php
@@ -16,38 +16,41 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <!-- My CSS -->
   <link rel="stylesheet" href="/htdocsDirectories/lar_tcc/assets/css/construction.css">
-  <link rel="stylesheet" href="/htdocsDirectories/lar_tcc/assets/css/construction.css">
   <link rel="stylesheet" href="/htdocsDirectories/lar_tcc/assets/fonts/stylesheet.css">
   <title>Página de Login</title>
 </head>
-<body>
+<body class="d-flex position-absolute top-50 start-50 translate-middle">
   <div class="d-flex justify-content-center align-items-center">
     <img src="../assets/images/background/House bookshelves 1.png">
-    <div class="d-flex justify-content-center align-items-center">
-      <div class="px-5">
+  </div>
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="px-5">
+      <div class="mb-5">
         <div class="d-flex justify-content-center">
-          <img src="../assets/images/logo.svg" style="width: 350px;">
+          <img src="../assets/images/logo.svg" style="width: 400px;">
         </div>
-        <div class="d-flex justify-content-center ">
+        <div class="d-flex justify-content-center">
           <h5 class="txt-red">Voltar para a <a href="/htdocsDirectories/lar_tcc/index.php" class="text-decoration-none txt-brown fw-bold"> Página inicial</a></h5>
         </div>
-        <div class="d-flex justify-content-center mt-2 txt-red">
-          <h1 class="fw-bold fontGeneral">FAÇA SEU LOGIN</h1>
-        </div>
-        <?php if($is_login_incorrect == true) { ?>
-          <div class="d-flex justify-content-center">
-            <h3>E-mail ou senha incorretos!</h3>
-          </div>
-        <?php } ?>
-        <div class="d-flex flex-column justify-content-center align-items-center px-5">
-          <form action="./login.php" method="post" class="d-flex flex-column justify-content-center align-items-center">
-            <input type="hidden" id="" name="operation" value="login">
-            <input type="text" id="userEmailInput" name="user_email" placeholder="Digite seu e-mail" class="rounded-4 bg-red border-red txt-brown fw-bold mb-3"> 
-            <input type="password" id="userPasswordInput" name="user_password" placeholder="Digite sua senha" class="rounded-4 bg-red border-red txt-brown fw-bold mb-3">
-            <input id="" type="submit" value="Entrar" class="border-0 bg-brown rounded-4 h5 py-3 px-5 fw-bold txt-pink">
-          </form>
-          <a href="/htdocsDirectories/lar_tcc/views/signup.php" class="text-decoration-none txt-brown fw-bold fs-5"> Não tenho conta </a>
       </div>
+      <div class="d-flex justify-content-center mt-2 txt-red">
+        <h1 class="fw-bold fontGeneral">FAÇA SEU LOGIN</h1>
+      </div>
+      <?php if($is_login_incorrect == true) { ?>
+        <div class="d-flex justify-content-center">
+          <h3>E-mail ou senha incorretos!</h3>
+        </div>
+      <?php } ?>
+      <div class="">
+        <form action="./login.php" method="post" class="d-flex flex-column justify-content-center align-items-center">
+          <input type="hidden" id="" name="operation" value="login">
+          <input type="text" id="userEmailInput" name="user_email" placeholder="Digite seu e-mail" class="rounded-5 bg-red border-red txt-brown fw-bold mb-4 py-2 w-100 fs-5"> 
+          <input type="password" id="userPasswordInput" name="user_password" placeholder="Digite sua senha" class="rounded-5 bg-red border-red txt-brown fw-bold mb-4 py-2 w-100 fs-5">
+          <input id="" type="submit" value="Entrar" class="border-0 bg-brown rounded-4 h5 py-3 px-5 fw-bold txt-pink">
+        </form>
+        <div class="d-flex justify-content-center">
+          <a href="/htdocsDirectories/lar_tcc/views/signup.php" class="text-decoration-none txt-brown fw-bold fs-5"> Não tenho conta </a>
+        </div>
       </div>
     </div>
   </div>
--- views/signup.php
+++ views/signup.php
@@ -16,40 +16,40 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <!-- My CSS -->
   <link rel="stylesheet" href="/htdocsDirectories/lar_tcc/assets/css/construction.css">
+  <link rel="stylesheet" href="/htdocsDirectories/lar_tcc/assets/fonts/stylesheet.css">
   <title>Página de Criação de Conta</title>
 </head>
-<body>
+<body class="d-flex position-absolute top-50 start-50 translate-middle">
   <div class="d-flex justify-content-center align-items-center">
-    <div class="">
-      <img src="../assets/images/background/House bookshelves 1.png">
-    </div>
-    <div class="d-flex justify-content-center align-items-center">
-      <div class="px-5">
+    <img src="../assets/images/background/House bookshelves 1.png">
+  </div>
+  <div class="d-flex justify-content-center align-items-center">
+    <div class="px-5">
+      <div class="mb-5">
         <div class="d-flex justify-content-center">
-          <img src="../assets/images/logo.svg" style="width: 350px;">
+          <img src="../assets/images/logo.svg" style="width: 400px;">
         </div>
         <div class="d-flex justify-content-center ">
           <h5 class="txt-red">Voltar para a <a href="/htdocsDirectories/lar_tcc/" class="text-decoration-none txt-brown fw-bold"> Página inicial</a></h5>
         </div>
-        <div class="d-flex justify-content-center mt-2 txt-red">
-          <h1>Crie sua conta!</h1>
+      </div>
+      <div class="d-flex justify-content-center mt-2 txt-red">
+        <h1 class="fw-bold fontGeneral">Crie sua conta!</h1>
+      </div>
+      <?php if($is_signup_incorrect == true) { ?>
+        <div class="d-flex justify-content-center">
+          <h3>E-mail já cadastrado</h3>
         </div>
-        <?php if($is_signup_incorrect == true) { ?>
-          <div class="d-flex justify-content-center">
-            <h3>E-mail já cadastrado</h3>
-          </div>
-        <?php } ?>
-        <!-- <div > -->
-          <form action="./signup.php" method="post" class="d-flex flex-column justify-content-center align-items-center px-5">
+      <?php } ?>
+      <div class="">
+        <form action="./signup.php" method="post" class="d-flex flex-column justify-content-center align-items-center px-5">
           <input type="hidden" name="operation" value="signup">
-            <input type="text" id="userFirstNameInput" name="first_name" placeholder="Digite seu nome!" class="rounded-4 bg-red border-red txt-brown fw-bold mb-3">
-            <input type="text" id="userLastNameInput" name="last_name" placeholder="Sobrenome" class="rounded-4 bg-red border-red txt-brown fw-bold mb-3">
-            <input type="text" id="userEmailInput" name="user_email" placeholder="Digite seu E-Mail!" class="rounded-4 bg-red border-red txt-brown fw-bold mb-3">
-            <input type="password" id="userPasswordInput" name="user_password" placeholder="Digite sua senha!" class="rounded-4 bg-red border-red txt-brown fw-bold mb-3">
-            <!-- <div class="d-grid p-0 mt-3"> -->
-              <input id="" type="submit" value="Criar Conta" class="border-0 bg-brown rounded-4 h5 p-3 px-5 fw-bold txt-pink">
-            <!-- </div> -->
-          </form>
+          <input type="text" id="userFirstNameInput" name="first_name" placeholder="Digite seu nome!" class="rounded-5 bg-red border-red txt-brown fw-bold mb-4 py-2 w-100 fs-5">
+          <input type="text" id="userLastNameInput" name="last_name" placeholder="Sobrenome" class="rounded-5 bg-red border-red txt-brown fw-bold mb-4 py-2 w-100 fs-5">
+          <input type="text" id="userEmailInput" name="user_email" placeholder="Digite seu E-Mail!" class="rounded-5 bg-red border-red txt-brown fw-bold mb-4 py-2 w-100 fs-5">
+          <input type="password" id="userPasswordInput" name="user_password" placeholder="Digite sua senha!" class="rounded-5 bg-red border-red txt-brown fw-bold mb-4 py-2 w-100 fs-5">
+          <input id="" type="submit" value="Criar Conta" class="border-0 bg-brown rounded-4 h5 p-3 px-5 fw-bold txt-pink">
+        </form>
         <div class="d-flex justify-content-center mt-2">
           <a href="/htdocsDirectories/lar_tcc/views/login.php" class="text-decoration-none txt-brown fw-bold fs-5"> Já tenho uma conta </a>
         </div>
